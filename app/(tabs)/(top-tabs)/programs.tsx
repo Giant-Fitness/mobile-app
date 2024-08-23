6fdb91342db51e439655429c6359470f552a57c8
Nikhil Wahi <nikhilwahi1995@gmail.com>
2024-08-23 23:21:39 +0530

    Updating styling and temporarily removing top tabs

--- app/(tabs)/(top-tabs)/programs.tsx
+++ app/(tabs)/(top-tabs)/programs.tsx
@@ -3,12 +3,21 @@
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { StatusBar } from 'expo-status-bar';
+import { StyleSheet } from 'react-native';
 import React from 'react';
 
 export default function ProgramsScreen() {
     return (
-        <ThemedView style={{ justifyContent: 'center', alignItems: 'center' }}>
+        <ThemedView style={styles.container}>
             <ThemedText>Programs</ThemedText>
         </ThemedView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
--- app/(tabs)/(top-tabs)/workouts.tsx
+++ app/(tabs)/(top-tabs)/workouts.tsx
@@ -29,6 +29,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         padding: 16,
+        flex: 1,
     },
     linkButton: {
         marginTop: 16,
