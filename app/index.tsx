b52d35d26cbf1c05912270bde43846d82a458403
kushar77 <kusharora339@gmail.com>
2024-09-01 19:40:16 -0700

    Auth wip

--- app/index.tsx
+++ app/index.tsx
@@ -1,37 +1,75 @@
 // app/index.tsx
 
 import React from 'react';
-import { StyleSheet, Pressable, Text } from 'react-native';
-import { ThemedView } from '@/components/base/ThemedView';
+import { Button, View, StyleSheet, SafeAreaView, Pressable, Text } from 'react-native';
+import { Amplify } from 'aws-amplify';
+import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react-native';
 import { Link } from 'expo-router';
 
-export default function LoginPage() {
+Amplify.configure({
+    Auth: {
+        Cognito: {
+            userPoolId: 'ap-south-1_IGoQb5wRf',
+            userPoolClientId: '60ps5arjs511dffmcu3arq902m',
+            identityPoolId: '<your-cognito-identity-pool-id>',
+            loginWith: {
+                email: true,
+                username: true,
+            },
+            signUpVerificationMethod: 'code',
+            userAttributes: {
+                email: {
+                    required: true,
+                },
+            },
+            allowGuestAccess: true,
+            passwordFormat: {
+                minLength: 8,
+                requireLowercase: true,
+                requireUppercase: true,
+                requireNumbers: true,
+                requireSpecialCharacters: true,
+            },
+        },
+    },
+});
+
+
+const SignOutButton = () => {
+    const { signOut } = useAuthenticator();
+
     return (
-        <ThemedView style={styles.titleContainer}>
-            <Link href={'/home'} replace asChild>
-                <Pressable style={styles.button}>
-                    <Text style={styles.buttonText}>Login</Text>
-                </Pressable>
-            </Link>
-        </ThemedView>
+        <View style={styles.signOutButton}>
+            <Button title='Sign Out' onPress={signOut} />
+        </View>
     );
-}
+};
+
+const LoginPage = () => {
+    return (
+        <Authenticator.Provider>
+            <Authenticator loginMechanisms={['email']}>
+                <SafeAreaView>
+                    <SignOutButton />
+                </SafeAreaView>
+            </Authenticator>
+        </Authenticator.Provider>
+    );
+};
 
 const styles = StyleSheet.create({
-    titleContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        gap: 8,
+    signOutButton: {
+        alignSelf: 'flex-end',
     },
     button: {
         paddingVertical: 10,
         paddingHorizontal: 20,
-        backgroundColor: '#007bff',
         borderRadius: 5,
     },
     buttonText: {
-        color: '#fff',
         fontSize: 16,
         fontWeight: 'bold',
     },
 });
+
+export default LoginPage;
