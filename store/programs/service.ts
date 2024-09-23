c5d82f8be4332ecb2e04136782e74b6d8d289ad4
Nikhil Wahi <nikhilwahi1995@gmail.com>
2024-09-23 17:54:43 +0530

    Simulating network delays in services

--- store/programs/service.ts
+++ store/programs/service.ts
@@ -3,34 +3,47 @@
 import { Program, ProgramDay, UserProgramProgress } from '@/types';
 import { sampleProgramDays, mockPrograms, sampleUserProgress } from '@/store/programs/mockData';
 
+// Utility function to simulate network delay
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+// Simulate a random delay between 200ms and 1000ms
+const simulateNetworkDelay = async () => {
+    const randomDelay = Math.floor(Math.random() * (1000 - 200 + 1) + 200);
+    await delay(randomDelay);
+};
+
 const getUserProgramProgress = async (): Promise<UserProgramProgress> => {
+    await simulateNetworkDelay();
     return sampleUserProgress;
 };
 
 const getAllPrograms = async (): Promise<Program[]> => {
+    await simulateNetworkDelay();
     return mockPrograms;
 };
 
 const getProgram = async (programId: string): Promise<Program | undefined> => {
+    await simulateNetworkDelay();
     return mockPrograms.find((program) => program.ProgramId === programId);
 };
 
 const getProgramDay = async (programId: string, dayId: string): Promise<ProgramDay | undefined> => {
+    await simulateNetworkDelay();
     return sampleProgramDays.find((day) => day.ProgramId === programId && day.DayId === dayId);
 };
 
 const getProgramDaysAll = async (programId: string): Promise<ProgramDay[]> => {
+    await simulateNetworkDelay();
     const allDays = sampleProgramDays.filter((day) => day.ProgramId === programId);
     return allDays;
 };
 
 const getProgramDaysFiltered = async (programId: string, dayIds: string[]): Promise<ProgramDay[]> => {
+    await simulateNetworkDelay();
     // get all the days for a program
     const allDays = sampleProgramDays.filter((day) => day.ProgramId === programId);
-
     // Filter only the days with IDs in dayIds
     const filteredDays = allDays.filter((day) => dayIds.includes(day.DayId));
-
     // Assuming the result should be sorted by DayId
     return filteredDays.sort((a, b) => parseInt(a.DayId) - parseInt(b.DayId));
 };
