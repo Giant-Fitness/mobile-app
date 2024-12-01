d067199710f0877ddb2b644ae443fbc31c32b8ba
Nikhil Wahi <nikhilwahi1995@gmail.com>
2024-12-02 02:20:01 +0530

    Adding support for multiple api endpoints

--- store/workouts/service.ts
+++ store/workouts/service.ts
@@ -1,13 +1,13 @@
 // store/workouts/service.ts
 
 import { Workout, WorkoutRecommendations } from '@/types';
-import { authApiClient } from '@/utils/api/apiConfig';
+import { authCatalogApiClient, authRecommendationsApiClient } from '@/utils/api/apiConfig';
 import { handleApiError } from '@/utils/api/errorUtils';
 
 const getAllWorkouts = async (): Promise<Workout[]> => {
     console.log('service: getAllWorkouts');
     try {
-        const { data } = await authApiClient.get('/workouts');
+        const { data } = await authCatalogApiClient.get('/workouts');
         return data.workouts || [];
     } catch (error) {
         throw handleApiError(error, 'GetAllWorkouts');
@@ -17,7 +17,7 @@ const getAllWorkouts = async (): Promise<Workout[]> => {
 const getWorkout = async (workoutId: string): Promise<Workout | undefined> => {
     console.log('service: getWorkout');
     try {
-        const { data } = await authApiClient.get(`/workouts/${workoutId}`);
+        const { data } = await authCatalogApiClient.get(`/workouts/${workoutId}`);
         return data.workout;
     } catch (error) {
         throw handleApiError(error, 'GetWorkout');
@@ -27,7 +27,7 @@ const getWorkout = async (workoutId: string): Promise<Workout | undefined> => {
 const getWorkouts = async (workoutIds: string[]): Promise<Workout[]> => {
     console.log('service: getWorkouts');
     try {
-        const { data } = await authApiClient.get('/workouts/batch', {
+        const { data } = await authCatalogApiClient.get('/workouts/batch', {
             params: {
                 workoutIds: workoutIds.join(','),
             },
@@ -41,7 +41,7 @@ const getWorkouts = async (workoutIds: string[]): Promise<Workout[]> => {
 const getSpotlightWorkouts = async (): Promise<WorkoutRecommendations> => {
     console.log('service: getSpotlightWorkouts');
     try {
-        const { data } = await authApiClient.get('/recommendations/workouts/spotlight');
+        const { data } = await authRecommendationsApiClient.get('/recommendations/workouts/spotlight');
         return data;
     } catch (error) {
         throw handleApiError(error, 'GetSpotlightWorkouts');
